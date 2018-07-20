e7d2961a1dd9c3187f57f29664e05430dddfca0f
Arun Drelich <arun_drelich@brown.edu>
2018-07-20 17:06:34 -0400

    pass arbitrary params to Nominatim API

--- OSMPythonTools/nominatim.py
+++ OSMPythonTools/nominatim.py
@@ -3,14 +3,17 @@ import urllib.parse
 from OSMPythonTools.internal.cacheObject import CacheObject
 
 class Nominatim(CacheObject):
-    def __init__(self, endpoint='https://nominatim.openstreetmap.org/search', **kwargs):
+    def __init__(self, endpoint='https://nominatim.openstreetmap.org/search', params={}, **kwargs):
         super().__init__('nominatim', endpoint, **kwargs)
+        self._params = params
     
     def _queryString(self, query):
         return (query, query)
     
     def _queryRequest(self, endpoint, queryString):
-        return endpoint + '?format=json&q=' + urllib.parse.quote_plus(queryString, safe='')
+        paramsDict = {'format':'json', 'q':queryString, **self._params}
+        queryParams = urllib.parse.urlencode(paramsDict, safe='')
+        return "{}?{}".format(endpoint, queryParams)
     
     def _rawToResult(self, data, queryString):
         return NominatimResult(data, queryString)

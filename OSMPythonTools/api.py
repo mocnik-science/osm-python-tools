400517fadd83c0d6277289ebc687f8b9c30a3b41
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2025-07-05 01:04:59 +0200

    code cleanup

--- OSMPythonTools/api.py
+++ OSMPythonTools/api.py
@@ -7,14 +7,14 @@ from OSMPythonTools.internal.singletonApi import SingletonApi
 class Api(CacheObject):
     def __init__(self, endpoint='http://www.openstreetmap.org/api/0.6/', **kwargs):
         super().__init__('api', endpoint, jsonResult=False, **kwargs)
-    
+
     def _queryString(self, query, params={}, history=False):
         query = query + ('/history' if history else '')
         return (query, query, params)
-    
+
     def _queryRequest(self, endpoint, queryString, params={}):
         return endpoint + queryString
-    
+
     def _rawToResult(self, data, queryString, params, kwargs, cacheMetadata=None, shallow=False):
         return ApiResult(data, queryString, params, cacheMetadata=cacheMetadata, shallow=shallow, history=kwargs['history'] if 'history' in kwargs else False)
 
@@ -33,24 +33,24 @@ class ApiResult(Element):
         super().__init__(cacheMetadata, soup=soup, soupHistory=soupHistory if history else None, shallow=shallow)
         self._queryString = queryString
         self._params = params
-    
+
     def _unshallow(self):
         api = SingletonApi()
         x = api.query(self.type() + '/' + str(self.id()) + ('/full' if self.type() != 'node' else ''))
         self.__init__(x._xml, x._queryString, x._params)
-    
+
     def isValid(self):
         return self._isValid
-    
+
     def toXML(self):
         return self._xml
-    
+
     def queryString(self):
         return self._queryString
-    
+
     def __get(self, prop):
         return self._soup2.attrs[prop] if self._isValid and prop in self._soup2.attrs else None
-    
+
     ### general information
     def apiVersion(self):
         return self.__get('version')

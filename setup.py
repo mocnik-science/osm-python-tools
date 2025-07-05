df4d0b4afaae78c34920f9a0193bf394f903036e
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2025-07-05 02:39:12 +0200

    new version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 pkgName='OSMPythonTools'
-pkgVersion='0.3.5'
+pkgVersion='0.3.6'
 pkgUrl='https://github.com/mocnik-science/osm-python-tools'
 
 with open('./OSMPythonTools/__info__.py', 'w') as f:
